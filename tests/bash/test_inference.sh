d7945026d8d85008aca1d1e6db5717a1069f5c84
lawrence-cj <cjs1020440147@icloud.com>
2024-12-04 17:47:15 +0800

    fix the bug of 600M online ckpt path;
    Signed-off-by: lawrence-cj <cjs1020440147@icloud.com>

--- tests/bash/test_inference.sh
+++ tests/bash/test_inference.sh
@@ -3,7 +3,7 @@ set -e
 
 python scripts/inference.py \
     --config=configs/sana_config/1024ms/Sana_600M_img1024.yaml \
-    --model_path=hf://Efficient-Large-Model/Sana_600M_1024px/checkpoints/Sana_600M_1024px.pth
+    --model_path=hf://Efficient-Large-Model/Sana_600M_1024px/checkpoints/Sana_600M_1024px_MultiLing.pth
 
 
 python scripts/inference.py \
