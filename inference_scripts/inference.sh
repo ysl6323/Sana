f5584bce6282c0eb62cc44c3998eb9121ffa4c91
ysl6323 <u202114051@hust.edu.cn>
2024-12-17 16:23:24 +0800

    modification

--- inference_scripts/inference.sh
+++ inference_scripts/inference.sh
@@ -1,5 +1,5 @@
 yaml=/data/shanglinyuan/Mini-Sana/configs/sana_config/512ms/Sana_200M_img512.yaml
-pth=/data/shanglinyuan/Mini-Sana/output/debug/checkpoints/epoch_77_step_50000.pth
+pth=/data/shanglinyuan/Mini-Sana/output/debug/checkpoints/epoch_1_step_626.pth
 prompt=/data/shanglinyuan/Mini-Sana/test_prompt.txt
 
 python scripts/inference.py\
